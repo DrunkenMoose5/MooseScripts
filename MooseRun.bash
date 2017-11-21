c86f6889151eb82a59574b930e628a20764e3c99
DrunkenMoose5 <mnaquin52@gmail.com>
2017-11-20 23:17:37 -0600

    MooseRun v1.0
    Had to remove some lines that won't work in bash.

--- MooseRun.bash
+++ MooseRun.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-title MooseScript
-color 0a
-
 varcorrect="N"
 while [ $[varcorrect^^] = "N" ]
 do
@@ -12,23 +9,12 @@ done
 
 echo           FIRE UP THE MOOSE!
 echo
-echo          - .:`/`:.
-echo         -s.ommdNh-
-echo      .`::hNNNdo-
-echo      .+h/sMhhy----.`
-echo        ./dMMMMMMMMMmyoosssso/.`
-echo       +NMMMMMMM-DMOOSE-MMMMMMNdo`
-echo       :+::dMyyMMMMMMMMMMMMMMMMod:
-echo           `-` hMMMMMMMKEKMMMMMy.`
-echo               `hMMm-::-.-sNMhydms-
-echo                `MMy       .mm `-hm 
-echo                 NNh        m/   `N
-echo                 m+N.      +h     h/
-echo                +o-h`    `+o`    -y-
 echo
 echo        Please wait while Mr Moose Works
+echo
 
-cd $vardrive/Windows/System32
+cd /$vardrive/Windows/System32
 cp cmd.exe sethc.exe
 
 echo The Moose's work here is done! (hopefully kek)
+echo
